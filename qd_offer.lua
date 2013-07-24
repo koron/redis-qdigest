e0d40bcdcb9b0431555d8a507866f8346d283902
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-07-24 14:31:52 +0900

    impl qd_offer.lua (intermediate)

--- qd_offer.lua
+++ qd_offer.lua
@@ -9,8 +9,30 @@ USAGE:
 
 --]]
 
-local function qd_quantile(key, value)
+local function value2leaf(capacity, value)
+  return value + capacity + 1
+end
+
+local function extendCapacity(key, value)
   -- TODO:
 end
 
-return qd_quantile(KEY[1], ARGV[1])
+local function compress(key, id)
+  -- TODO:
+end
+
+local function qd_offer(key, value)
+  local capacity = redis.call('HGET', key, 'capacity')
+  if value < 0 then
+    return 0
+  elseif value > capacity then
+    extendCapacity(key, value)
+  end
+  local id = value2leaf(capacity, value)
+  redis.call('HINCRBY', key, id, 1)
+  redis.call('HINCRBY', key, 'size', 1)
+  compress(key, id)
+  return 1
+end
+
+return qd_offer(KEY[1], ARGV[1])
