f908cacfd04854b714ef37317b1ef18057a15112
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-08-14 12:05:29 +0900

    write documents

--- src/main/java/net/kaoriya/qb/redis_qdigest/QDigest.java
+++ src/main/java/net/kaoriya/qb/redis_qdigest/QDigest.java
@@ -4,8 +4,24 @@ import java.lang.ref.WeakReference;
 
 import redis.clients.jedis.Jedis;
 
+/**
+ * QDigest implementation which using Redis as backend.
+ */
 public final class QDigest
 {
+    /**
+     * Get QDigest instance by key and factor.
+     *
+     * When there are no exsiting QDigest by key, this create new one and
+     * return it.  When there is an existing QDigest by key and it have same
+     * factor, this return it.
+     *
+     * Otherwise return null.
+     *
+     * @param jedis Connection to Redis.
+     * @param key Name of QDigest.  Which is used as a key in Redis.
+     * @param factor Memory usage factor.
+     */
     public static QDigest getInstance(Jedis jedis, String key, long factor)
         throws QDigestException
     {
@@ -20,6 +36,12 @@ public final class QDigest
         }
     }
 
+    /**
+     * Drop QDigest instance from Redis.
+     *
+     * @param jedis Connection to Redis.
+     * @param key Name of QDigest.
+     */
     public static void dropInstance(Jedis jedis, String key)
         throws QDigestException
     {
@@ -34,18 +56,31 @@ public final class QDigest
         this.key = key;
     }
 
+    /**
+     * Offer a value.
+     *
+     * @param value Value to offer.
+     */
     public boolean offer(long value)
         throws QDigestException
     {
         return QDigestAdapter.offer(this.jedis.get(), this.key, value);
     }
 
+    /**
+     * Get quantile of values.
+     *
+     * @param q Fraction of quantile.
+     */
     public long quantile(float q)
         throws QDigestException
     {
         return QDigestAdapter.quantile(this.jedis.get(), this.key, q);
     }
 
+    /**
+     * Drop data related to QDigest from Redis.
+     */
     public void drop()
         throws QDigestException
     {
